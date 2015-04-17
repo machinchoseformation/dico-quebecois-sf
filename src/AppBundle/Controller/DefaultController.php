dcb5463fe04cdd9c28c54e7dada3ce62cc292bfc
guillaume <gsylvestre@gmail.com>
2015-04-17 16:32:56 +0200

    add dico to all views, details page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Term;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -13,5 +14,26 @@ class DefaultController extends Controller
     public function homeAction()
     {
         return $this->render('default/home.html.twig');
+    }
+
+    /**
+     * @Route("/{slug}/definition", name="term")
+     */
+    public function termAction(Term $term)
+    {
+        $params = array();
+        $params['term'] = $term;
+        return $this->render('default/term.html.twig', $params);
+    }
+
+    /**
+     * @Route("/dico", name="dico")
+     */
+    public function dicoAction()
+    {
+        $termRepo = $this->getDoctrine()->getRepository("AppBundle:Term");
+        $dico = $termRepo->findBy(array(), array("name" => "ASC"));
+        $params = array("dico" => $dico);
+        return $this->render('default/dico.html.twig', $params);
     }
 }
